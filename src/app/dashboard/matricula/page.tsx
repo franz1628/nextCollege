30966ae3d3065c0fc03d86f39e14312633730dec
Franz Wilder <gualambo@gmail.com>
2025-03-22 23:18:36 -0500

    add getCursos

--- src/app/dashboard/matricula/page.tsx
+++ src/app/dashboard/matricula/page.tsx
@@ -1,8 +1,26 @@
+'use client';
+import apiClient from "@/lib/axios/apiClient";
+import { CursoService } from "@/services/CursoService";
+import { CursoModel } from "@/types/CursoModel";
+import { get } from "https";
+import { useEffect, useState } from "react";
+
 interface Props{
 
 }
 
 const Matricula:React.FC<Props> = () => {
+    const [cursos, setCursos] = useState<CursoModel[]>([]);
+
+    useEffect(()=>{
+        getCursos();
+    },[]);
+
+    const getCursos = async () => {
+      const response = await CursoService.get();
+      console.log(response);
+    }
+
     return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold text-gray-800 mb-6">Matrícula</h1>
